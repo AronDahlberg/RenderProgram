49d6a6d88d92a9972c5459c2c6d656fc1be92ca2
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-14 11:55:16 +0200

    Add project files.

---
+++ RenderProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RenderProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
