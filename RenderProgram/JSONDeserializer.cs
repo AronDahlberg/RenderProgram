5762f89f55f5f39d1a62c72a0c49cc30d884f47f
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-14 16:15:42 +0200

    Added JSON Deserializer

---
+++ RenderProgram/JSONDeserializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RenderProgram
+{
+    internal static class JSONDeserializer
+    {
+        public static AppSettings ReadSettings(string filePath)
+        {
+            return JsonSerializer.Deserialize<AppSettings>(File.ReadAllText(filePath));
+        }
+    }
+    public class Option
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Text
+    {
+        public List<Option> Options { get; set; }
+        public string Header { get; set; }
+    }
+
+    public class Menu
+    {
+        public int Id { get; set; }
+        public Text Text { get; set; }
+    }
+
+    public class AppSettings
+    {
+        public List<Menu> Menus { get; set; }
+    }
+}
