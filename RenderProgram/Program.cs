800082849152ca054c2beec6c52c7998e665ee4f
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-14 16:09:08 +0200

    Changed WriteMenu method

--- RenderProgram/Program.cs
+++ RenderProgram/Program.cs
@@ -3,11 +3,13 @@
     internal class Program
     {
         private static bool IsRunning { get; set; } = true;
+        private static int CurrentMenu { get; set; } = 0;
+        private static AppSettings Settings { get; set; }
         static void Main(string[] args)
         {
             while (IsRunning)
             {
-                WriteMenu();
+                WriteMenu(CurrentMenu);
                 string? input = Console.ReadLine();
 
                 switch (input)
@@ -16,9 +18,26 @@
                 }
             }
         }
-        private static void WriteMenu()
+        private static void WriteMenu(int option)
         {
+            Console.Clear();
 
+            var selectedMenu = Settings.Menus.FirstOrDefault(menu => menu.Id == option);
+
+            // Print the header if it exists
+            if (selectedMenu.Text?.Header != null)
+            {
+                Console.WriteLine(selectedMenu.Text.Header);
+            }
+
+            // Print the options if they exist
+            if (selectedMenu.Text?.Options != null)
+            {
+                foreach (var menuOption in selectedMenu.Text.Options)
+                {
+                    Console.WriteLine($"{menuOption.Id}: {menuOption.Text}");
+                }
+            }
         }
     }
 }
