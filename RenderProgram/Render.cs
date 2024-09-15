cbd949c1569e35b5f7035f1e70c1195d6d548741
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-15 13:04:39 +0200

    Added Render class

---
+++ RenderProgram/Render.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderProgram
+{
+    internal static class Render
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
