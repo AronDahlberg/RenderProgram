7d488f911a5a837a32ca02e5c6893a767c1426fc
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-15 14:42:28 +0200

    Added FPS cap

--- RenderProgram/Render.cs
+++ RenderProgram/Render.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,72 @@ namespace RenderProgram
 {
     internal static class Render
     {
-        public static void Run()
+        private static bool IsRunning { get; set; }
+        private static double FrameTime { get; set; }
+        private static AutoResetEvent TimingSignal { get; } = new(false);
+        private static Stopwatch Watch { get; } = new();
+        private static char[,] Frame {  get; set; }
+        public static async void Run()
+        {
+            FrameTime = 1000.0 / Program.Settings.GeneralParameters.Parameters.FirstOrDefault(parameter => parameter.Name == "Fps").Value;
+
+            Task handleInputTask = Task.Run(() => HandleInput());
+            Task timingTask = Task.Run(() => FPSControlLoop());
+
+            while (IsRunning)
+            {
+                // Wait for the timing signal to start the next frame
+                TimingSignal.WaitOne();
+
+                RenderFrame();
+                PrintFrame();
+            }
+
+            // Wait for all tasks to finish
+            await Task.WhenAll(handleInputTask,  timingTask);
+        }
+        private static void FPSControlLoop()
+        {
+            while (IsRunning)
+            {
+                Watch.Restart();
+
+                // Allow the render task to proceed (render and print the next frame)
+                TimingSignal.Set();
+
+                // Cap the FPS
+                Watch.Stop();
+                double elapsedMilliseconds = Watch.Elapsed.TotalMilliseconds;
+                double sleepTime = FrameTime - elapsedMilliseconds;
+
+                if (sleepTime > 0)
+                {
+                    Thread.Sleep((int)sleepTime);
+                }
+            }
+        }
+        private static void HandleInput()
+        {
+            while (IsRunning)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+
+                    switch (key.Key)
+                    {
+                        //keybinds
+                    }
+                }
+
+                Thread.Sleep(10);
+            }
+        }
+        private static void RenderFrame()
+        {
+
+        }
+        private static void PrintFrame()
         {
 
         }
